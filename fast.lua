0139c21695720b4f930a8147468a15f549c394e0
CopiluCuSarmale <96202219+ChaosHunterOK@users.noreply.github.com>
2025-08-25 19:16:10 +0300

    small update on fast

--- fast.lua
+++ fast.lua
@@ -1,82 +1,77 @@
 local love = require("love")
-local lg = love.graphics
+local lg, la, lt, lw, lm = love.graphics, love.audio, love.timer, love.window, love.math
+
 local fast = {
     cache = {
         images = {},
         fonts = {},
         sounds = {}
     },
-    vsync = true,
-    fpsCap = 0
+    vsync  = true,
+    fpsCap = 0,
+    _lastTime = lt.getTime()
 }
 
-function fast.getImage(path)
-    if not fast.cache.images[path] then
-        fast.cache.images[path] = lg.newImage(path)
-        fast.cache.images[path]:setFilter("nearest", "nearest")
+function fast.getImage(path, filter)
+    local img = fast.cache.images[path]
+    if not img then
+        img = lg.newImage(path)
+        img:setFilter(filter or "nearest", filter or "nearest")
+        fast.cache.images[path] = img
     end
-    return fast.cache.images[path]
+    return img
 end
 
 function fast.getFont(path, size)
-    local key = path .. tostring(size)
-    if not fast.cache.fonts[key] then
-        fast.cache.fonts[key] = lg.newFont(path, size)
+    local key = path .. ":" .. tostring(size)
+    local f = fast.cache.fonts[key]
+    if not f then
+        f = lg.newFont(path, size)
+        fast.cache.fonts[key] = f
     end
-    return fast.cache.fonts[key]
+    return f
 end
 
 function fast.getSound(path, type)
     type = type or "static" -- "static" or "stream"
-    if not fast.cache.sounds[path] then
-        fast.cache.sounds[path] = love.audio.newSource(path, type)
+    local s = fast.cache.sounds[path]
+    if not s then
+        s = la.newSource(path, type)
+        fast.cache.sounds[path] = s
     end
-    return fast.cache.sounds[path]:clone()
+    return s:clone()
 end
 
-function fast.randomColor()
-    return love.math.random(), love.math.random(), love.math.random(), 1
+function fast.randomColor(a)
+    return lm.random(), lm.random(), lm.random(), a or 1
 end
 
-function fast.hexColor(hex)
+function fast.hexColor(hex, a)
     hex = hex:gsub("#","")
-    return tonumber("0x"..hex:sub(1,2))/255,
-           tonumber("0x"..hex:sub(3,4))/255,
-           tonumber("0x"..hex:sub(5,6))/255,
-           1
+    return tonumber(hex:sub(1,2), 16) / 255,
+           tonumber(hex:sub(3,4), 16) / 255,
+           tonumber(hex:sub(5,6), 16) / 255,
+           a or 1
 end
 
-local lastTime = 0
-function fast.sleep(dt)
+function fast.limitFPS()
     if fast.fpsCap > 0 then
         local target = 1 / fast.fpsCap
-        local sleepTime = target - (love.timer.getTime() - lastTime)
-        if sleepTime > 0 then love.timer.sleep(sleepTime) end
-        lastTime = love.timer.getTime()
+        local now = lt.getTime()
+        local sleepFor = target - (now - fast._lastTime)
+        if sleepFor > 0 then lt.sleep(sleepFor) end
+        fast._lastTime = lt.getTime()
     end
 end
 
 function fast.setVsync(on)
     fast.vsync = on and 1 or 0
-    love.window.setMode(0, 0, {vsync = fast.vsync})
-end
-
-local lastTime = love.timer.getTime()
-
-function fast.limitFPS()
-    if fast.fpsCap > 0 then
-        local target = 1 / fast.fpsCap
-        local now = love.timer.getTime()
-        local sleepTime = target - (now - lastTime)
-        if sleepTime > 0 then
-            love.timer.sleep(sleepTime)
-        end
-        lastTime = love.timer.getTime()
-    end
+    local w, h, flags = lw.getMode()
+    lw.setMode(w, h, {vsync = fast.vsync})
 end
 
 function fast.newBatch(image, max)
-    lg.newSpriteBatch(image, max or 2000)
+    return lg.newSpriteBatch(image, max or 2000)
 end
 
 return fast
