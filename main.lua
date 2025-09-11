d2c2307c0e6047b3b9b4f6c2bf7f3e2619050ba7
CopiluCuSarmale <96202219+ChaosHunterOK@users.noreply.github.com>
2025-09-11 20:00:56 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ love.graphics.setDefaultFilter("nearest", "nearest")
 local spritesFolder = "images/sprites/"
 local stats = {score = 0, rings = 0}
 local gameTime = 0
-local gamestate = "menuscreen"
+local gamestate = "warning"
 
 local isMobile = false
 local os_device = love.system.getOS()
